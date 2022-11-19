10c63a99cb5054ed45e2303d93d7a40941241ee0
stevenli-uoft <stevenli.cy@mail.utoronto.ca>
2022-11-19 15:40:13 -0500

    Fixed Github Autograding Workflow

--- src/main/java/database/MongoTesting.java
+++ src/main/java/database/MongoTesting.java
@@ -1,11 +1,6 @@
 package database;//package database;
 
-//import com.mongodb.*;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+import com.mongodb.*;
 
 import java.net.UnknownHostException;
 import java.util.Arrays;
@@ -14,41 +9,24 @@ import java.util.List;
 public class MongoTesting {
     public static void main(String[] args) throws UnknownHostException {
 
-        MongoClient mongoClient = MongoClients.create("mongodb://stevenli:stevenli@cluster0.koruj0t.mongodb.net/?retryWrites=true&w=majority");
-
-        MongoDatabase db = mongoClient.getDatabase("sampleDB");
-
-        MongoCollection collection = db.getCollection("sampleCollection");
-
-        Document sampledoc = new Document("_id", "1").append("name", "john smith");
-
-        collection.insertOne(sampledoc);
-
-
         //My local database
-//        MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+//        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
 
         //My remote database
-//        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://stevenli:stevenli@cluster0.koruj0t.mongodb.net/?retryWrites=true&w=majority"));
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://stevenli:stevenli@cluster0.koruj0t.mongodb.net/?retryWrites=true&w=majority"));
 
-        //other database
+        //Brians remote database
 //        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://123:123@cluster1.d3e1rhp.mongodb.net/?retryWrites=true&w=majority"));
 
 
-//        DB database = mongoClient.getDB("schedule6-testingdb");
-//
-//        DBCollection collection = database.getCollection("schedule6-testingcollection");
-//
-//        List<Integer> books = Arrays.asList(27464, 747854);
-//        DBObject person = new BasicDBObject("_id", "jo")
-//                .append("name", "Tony Stark")
-//                .append("address", new BasicDBObject("street", "123 Fake St")
-//                        .append("city", "Faketon")
-//                        .append("state", "MA")
-//                        .append("zip", 12345))
-//                .append("books", books);
-//
-//        collection.insert(person);
+        DB database = mongoClient.getDB("schedule6-testingdb");
+
+        DBCollection collection = database.getCollection("schedule6-testingcollection");
+
+        DBObject person = new BasicDBObject("_id", "Steven1234")
+                .append("name", "Bob the Builder");
+
+        collection.insert(person);
     }
 }
 
