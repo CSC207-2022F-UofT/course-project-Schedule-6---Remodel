8dffc32e24758f9969ee9c8a995d01bd6029508f
stevenli-uoft <stevenli.cy@mail.utoronto.ca>
2022-11-24 14:51:11 -0500

    Finishing feature 2

--- src/main/java/database/MongoDBAccess.java
+++ src/main/java/database/MongoDBAccess.java
@@ -3,10 +3,15 @@ package database;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoClient;
+import entity.CommonScheduleItem;
+import entity.CommonTask;
+import net.fortuna.ical4j.transform.calendar.CancelTransformer;
 import requestModel.ScheduleItemRequestModel;
+import requestModel.TaskRequestModel;
 import useCaseInteractor.DataAccess;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class MongoDBAccess implements DataAccess {
 
@@ -23,7 +28,17 @@ public class MongoDBAccess implements DataAccess {
     }
 
     @Override
-    public void pullfromDB() {
+    public ArrayList<CommonScheduleItem> getUserSchedule(ScheduleItemRequestModel requestModel) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<CommonTask> getUserTask(TaskRequestModel requestModel) {
+        return null;
+    }
+
+    @Override
+    public void deleteFromDB(ScheduleItemRequestModel requestModel) {
 
     }
 }
--- src/main/java/useCaseInteractor/DataAccess.java
+++ src/main/java/useCaseInteractor/DataAccess.java
@@ -1,11 +1,22 @@
 package useCaseInteractor;
 
 import boundary.AddScheduleItemInputBoundary;
+import com.mongodb.client.MongoCollection;
+import entity.CommonScheduleItem;
+import entity.CommonTask;
 import requestModel.ScheduleItemRequestModel;
+import requestModel.TaskRequestModel;
 import responseModel.ScheduleItemResponseModel;
+
+import java.util.ArrayList;
 
 public interface DataAccess {
     void savetoDB(ScheduleItemRequestModel requestModel);
 
-    void pullfromDB();
+    ArrayList<CommonScheduleItem> getUserSchedule(ScheduleItemRequestModel requestModel);
+
+    // should return some type of mongoDB, probably a collection
+    ArrayList<CommonTask> getUserTask(TaskRequestModel requestModel);
+
+    void deleteFromDB(ScheduleItemRequestModel requestModel);
 }
