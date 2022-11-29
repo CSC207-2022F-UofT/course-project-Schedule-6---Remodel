61899606e6509f9dccd8852bcf2c51e81d5040f7
nizarabdouss <nizarabdouss@gmail.com>
2022-11-29 17:22:25 -0500

    Started working on viewing friends

--- src/main/java/controller/Schedule/TimetableController.java
+++ src/main/java/controller/Schedule/TimetableController.java
@@ -3,6 +3,10 @@ package controller.Schedule;
 import com.calendarfx.view.CalendarView;
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarSource;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,9 +14,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
+import java.net.UnknownHostException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import database.MongoDBAccess;
+import useCaseInteractor.User.userCollection;
 
 
 public class TimetableController {
@@ -43,17 +51,32 @@ public class TimetableController {
 
 
 
-    private void loadCalendar() {
+    private void loadCalendar() throws UnknownHostException {
         calendar = new CalendarView();
 
-        Calendar classes = new Calendar("null");
-        Calendar meetings = new Calendar("Meetings");
 
-        classes.setStyle(Calendar.Style.STYLE7);
-        meetings.setStyle(Calendar.Style.STYLE2);
+        MongoDBAccess client = new MongoDBAccess(this.main(), userCollection.getUsername());
+
+        ArrayList<String> followers = (ArrayList<String>) client.getFollowing();
+
+        ArrayList<Calendar> calendars = new ArrayList<>();
+
+        for(String e : followers){
+            Calendar friends = new Calendar(e);
+            friends.setStyle(Calendar.Style.STYLE7);
+            calendars.add(friends);
+        }
+
+        //EVERYTHING BELOW IS STABLE
+        //Calendar classes = new Calendar("null");
+        //Calendar meetings = new Calendar("Meetings");
+
+        //classes.setStyle(Calendar.Style.STYLE7);
+        //meetings.setStyle(Calendar.Style.STYLE2);
 
         CalendarSource myCalendarSource = new CalendarSource("Timetable");
-        myCalendarSource.getCalendars().addAll(classes, meetings);
+        myCalendarSource.getCalendars().addAll(calendars);
+
 
         calendar.getCalendarSources().addAll(myCalendarSource);
 
@@ -91,8 +114,24 @@ public class TimetableController {
         Gridlock.getChildren().add(calendar);
     }
 
-    public void initialize(){
+
+    public void initialize() throws UnknownHostException {
         usernameChageLabel = TimetableUserName;
         loadCalendar();
+    }
+
+
+    public DBCollection main() throws UnknownHostException {
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://stevenli:stevenli@cluster0.koruj0t.mongodb.net/?retryWrites=true&w=majority"));
+
+        //Brians remote database
+//        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://123:123@cluster1.d3e1rhp.mongodb.net/?retryWrites=true&w=majority"));
+
+
+        DB database = mongoClient.getDB("schedule6-testingdb");
+        DBCollection collection = database.getCollection("schedule6-testingcollection");
+
+        //this.loadCalendar(collection);
+        return collection;
     }
 }
