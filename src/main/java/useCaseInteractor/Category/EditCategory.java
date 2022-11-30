48c9ce2024619fe775c4d794ed762be2186d6c3c
Bri-xX <brian.xue@mail.utoronto.ca>
2022-11-30 16:27:59 -0500

    Added EditCategory (switching to new branch due to new data structure)

--- src/main/java/useCaseInteractor/Category/EditCategory.java
+++ src/main/java/useCaseInteractor/Category/EditCategory.java
@@ -1,5 +1,22 @@
 package useCaseInteractor.Category;
 
-public class EditCategory {
+import presenter.EditCategoryPresenter;
+import requestModel.CategoryRequestModel;
+import responseModel.Category.CategoryResponseModel;
+import useCaseInteractor.DataAccess;
 
+public class EditCategory {
+    final DataAccess dataAccess;
+
+    final EditCategoryPresenter categoryPresenter;
+
+    public EditCategory(DataAccess dataAccess, EditCategoryPresenter categoryPresenter) {
+        this.dataAccess = dataAccess;
+        this.categoryPresenter = categoryPresenter;
+    }
+
+    public CategoryResponseModel edit(CategoryRequestModel newInputData, CategoryRequestModel oldInputData){
+
+        return null;
+    }
 }
