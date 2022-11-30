644cefaa31f032655108d26cee908636c786dfd6
Bri-xX <brian.xue@mail.utoronto.ca>
2022-11-29 23:44:46 -0500

    implemented CCResponseModel in AddCategoryController, nameChecker WIP

--- src/main/java/controller/Category/AddCategoryController.java
+++ src/main/java/controller/Category/AddCategoryController.java
@@ -2,6 +2,8 @@ package controller.Category;
 
 import boundary.Category.CategoryCreationInputBoundary;
 import presenter.AddCateogoryPresenter;
+import requestModel.CategoryCreationRequestModel;
+import responseModel.Category.CategoryCreationResponseModel;
 
 public class AddCategoryController {
     final  CategoryCreationInputBoundary categoryCreationInputBoundary;
@@ -13,5 +15,20 @@ public class AddCategoryController {
         this.presenter = presenter;
     }
 
+    public CategoryCreationResponseModel create(String name, boolean status) {
+        if (name.isBlank()) {
+            return presenter.prepareFailView("Please fill in category name");
+        } else if (!nameChecker(name)) {
+            return presenter.prepareFailView("Category already exists");
+        }
+        CategoryCreationRequestModel inputData = new CategoryCreationRequestModel(name, status);
+        return categoryCreationInputBoundary.create(inputData);
+    }
+
+    //need to implement nameChecker
+    public boolean nameChecker(String name) {
+
+        return true;
+    }
     //cont..
 }
