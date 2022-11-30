52fc0293b1cf101c795e7d421a0f182e07b094e7
Tristen <Tristenwhite541@hotmail.com>
2022-11-30 15:45:10 -0500

    changed the default calendar title to the username and added a label to the main page to tell users they have saved their data. Need to add a fadeout to the label to finish this branch.

--- src/main/java/controller/Schedule/TimetableController.java
+++ src/main/java/controller/Schedule/TimetableController.java
@@ -31,11 +31,15 @@ public class TimetableController {
             System.out.println(temp.findEntries(TM.getStartDate(), TM.getEndDate(), TM.getTimeZone()));
         }
     }
+    public void setUsernameChangeLabel(String name){
+        for (Calendar temp: calendar.getCalendars()) {
+            temp.setName(name);
+        }
+    }
     public void scheduleInputsButton(ActionEvent event){}
     
     private void loadCalendar() throws UnknownHostException {
         calendar = new CalendarView();
-
 
         MongoDBAccess client = new MongoDBAccess(this.main(), userCollection.getUsername());
 
@@ -56,12 +60,11 @@ public class TimetableController {
         //classes.setStyle(Calendar.Style.STYLE7);
         //meetings.setStyle(Calendar.Style.STYLE2);
 
-        CalendarSource myCalendarSource = new CalendarSource("Timetable");
+        CalendarSource myCalendarSource = new CalendarSource("");
         myCalendarSource.getCalendars().addAll(calendars);
-
-
         calendar.getCalendarSources().addAll(myCalendarSource);
-
+        System.out.println(calendar.getCalendarSources());
+        calendar.getCalendarSources().remove(1);
         calendar.setRequestedTime(LocalTime.now());
 
         Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
@@ -91,6 +94,7 @@ public class TimetableController {
         calendar.setShowAddCalendarButton(true);
         calendar.setShowPrintButton(false);
         calendar.setShowDeveloperConsole(false);
+        calendar.setShowAddCalendarButton(false);
         Gridlock.getChildren().add(calendar);
     }
 
--- src/main/java/screens/TimeTableScreen.java
+++ src/main/java/screens/TimeTableScreen.java
@@ -17,11 +17,14 @@ public class TimeTableScreen {
     private Button saveButton;
     @FXML
     private Button newEvent;
+    @FXML
+    private Label allEntriesSavedLabel;
     public static Label usernameChangeLabel;
 
     private TimetableController TTC = new TimetableController();
 
-    public void printCalendarEntries(ActionEvent event) {TTC.printCalendarEntries(event);}
+    public void printCalendarEntries(ActionEvent event) throws InterruptedException
+    {TTC.printCalendarEntries(event, allEntriesSavedLabel);}
     public void scheduleInputsButton(ActionEvent event) {TTC.scheduleInputsButton(event);}
 
     public void initialize(){
