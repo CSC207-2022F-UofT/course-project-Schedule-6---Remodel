824583b831c08dcd9ab58c1a30ab28704f7d9ab1
Bri-xX <brian.xue@mail.utoronto.ca>
2022-12-01 11:20:54 -0500

    deleted unneeded file

--- src/main/java/presenter/EditCategoryPresenter.java
+++
@@ -1,9 +0,0 @@
-package presenter;
-
-import responseModel.Category.CategoryResponseModel;
-
-public interface EditCategoryPresenter {
-    CategoryResponseModel prepareSuccessView(CategoryResponseModel categoryResponseModel);
-
-    CategoryResponseModel prepareFailView(String error);
-}
