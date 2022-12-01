e68567368905b7a3b49919fefc71aca9a0310899
Bri-xX <brian.xue@mail.utoronto.ca>
2022-12-01 11:18:26 -0500

    resolved errors related to the removal of category entity

--- src/main/java/entity/Task/TaskFactory.java
+++ src/main/java/entity/Task/TaskFactory.java
@@ -1,9 +1,7 @@
 package entity.Task;
 
-import entity.Category.Category;
-
 import java.time.LocalDate;
 
 public interface TaskFactory {
-    Task create(String title, LocalDate date, String privacySetting, Category category);
+    Task create(String title, LocalDate date, Boolean privacySetting, String category);
 }
