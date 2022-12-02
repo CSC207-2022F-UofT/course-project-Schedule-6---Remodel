6a5d3085af2ff9f52c9f0c907e02c0698258bfb9
Bri-xX <brian.xue@mail.utoronto.ca>
2022-12-02 15:57:20 -0500

    implemented DeleteTaskController

--- src/main/java/controller/Task/DeleteTaskController.java
+++ src/main/java/controller/Task/DeleteTaskController.java
@@ -1,4 +1,24 @@
 package controller.Task;
 
+import boundary.Task.DeleteTaskInputBoundary;
+import presenter.TaskPresenter;
+import requestModel.TaskRequestModel;
+import responseModel.Task.TaskResponseModel;
+
+import java.time.LocalDate;
+
 public class DeleteTaskController {
+    final DeleteTaskInputBoundary deleteTaskInputBoundary;
+
+    final TaskPresenter taskPresenter;
+
+    public DeleteTaskController(DeleteTaskInputBoundary deleteTaskInputBoundary, TaskPresenter taskPresenter) {
+        this.deleteTaskInputBoundary = deleteTaskInputBoundary;
+        this.taskPresenter = taskPresenter;
+    }
+
+    public TaskResponseModel delete(String title, LocalDate date, boolean privacySetting, String category) {
+        TaskRequestModel inputData = new TaskRequestModel(title, date, privacySetting, category);
+        return deleteTaskInputBoundary.delete(inputData);
+    }
 }
