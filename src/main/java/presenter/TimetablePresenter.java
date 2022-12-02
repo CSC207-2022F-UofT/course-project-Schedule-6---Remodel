4c6bb709fbb6bb7d1a3e229ede64cca4933785d8
nizarabdouss <nizarabdouss@gmail.com>
2022-12-02 18:43:39 -0500

    Working on breaking down map for schedule

--- src/main/java/presenter/TimetablePresenter.java
+++ src/main/java/presenter/TimetablePresenter.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.util.Duration;
 import entity.Schedule.TimeManagement;
 
+import java.util.Map;
 
 
 public class TimetablePresenter {
@@ -16,15 +17,23 @@ public class TimetablePresenter {
 
 
     public void printCalendarEntries(Label entriesSaved, CalendarView calendar) {
-        for (Calendar temp : calendar.getCalendars()) {
-            System.out.println(temp.findEntries(TM.getStartDate(), TM.getEndDate(), TM.getTimeZone()));
+        Calendar temp = calendar.getCalendars().get(0);
+        Map map = temp.findEntries(TM.getStartDate(), TM.getEndDate(), TM.getTimeZone());
+        this.cleanMap(map);
 
-            entriesSaved.setText("ALL ENTIRES SAVED");
-            FadeTransition ft = new FadeTransition(Duration.millis(1850), entriesSaved);
-            ft.setFromValue(1.0);
-            ft.setToValue(0.0);
-            ft.setAutoReverse(true);
-            ft.play();
-        }
+
+        entriesSaved.setText("ALL ENTIRES SAVED");
+        FadeTransition ft = new FadeTransition(Duration.millis(1850), entriesSaved);
+        ft.setFromValue(1.0);
+        ft.setToValue(0.0);
+        ft.setAutoReverse(true);
+        ft.play();
+    }
+
+    private void cleanMap(Map map){
+        Map newMap;
+        System.out.println(map);
+        System.out.println(map.keySet());
+        System.out.println(map.keySet().toArray());
     }
 }
