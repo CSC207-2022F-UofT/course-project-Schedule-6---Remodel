bcc1ac4e22fb3da5f821bb5cbaebb4403c1877b3
Tristen <Tristenwhite541@hotmail.com>
2022-12-02 22:01:27 -0500

    outputs the values of the map to give us the ability to add entries to the map using that data.

--- src/main/java/presenter/TimetablePresenter.java
+++ src/main/java/presenter/TimetablePresenter.java
@@ -16,6 +16,7 @@ import main.LoginPage;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.*;
 
 
 public class TimetablePresenter {
@@ -26,6 +27,52 @@ public class TimetablePresenter {
     public void printCalendarEntries(Label entriesSaved, CalendarView calendar) {
         for (Calendar temp : calendar.getCalendars()) {
             System.out.println(temp.findEntries(TM.getStartDate(), TM.getEndDate(), TM.getTimeZone()));
+            Map map = temp.findEntries(TM.getStartDate(), TM.getEndDate(), TM.getTimeZone());
+        int count = 1;
+        for (Object entry : map.values()) {
+
+            //Finds the title in the values of the map
+            int title_start = entry.toString().toUpperCase().indexOf("TITLE=") + ("TITLE=").length();
+            int title_end = entry.toString().toUpperCase().indexOf(",");
+            String title = entry.toString().substring(title_start, title_end);
+
+            //Finds the start date in the values of the map
+            int startDate_start = entry.toString().toUpperCase().indexOf("STARTDATE=") + ("STARTDATE=").length();
+            int startDate_end = startDate_start + 10;
+            String startDate = entry.toString().substring(startDate_start, startDate_end);
+
+            //Finds the end date in the values of the map
+            int endDate_start = entry.toString().toUpperCase().indexOf("ENDDATE=") + ("ENDDATE=").length();
+            int endDate_end = endDate_start + 10;
+            String endDate = entry.toString().substring(endDate_start, endDate_end);
+
+            //Finds the start time in the values of the map
+            int startTime_start = entry.toString().toUpperCase().indexOf("STARTTIME=") + ("STARTTIME=").length();
+            int startTime_end = startTime_start + 5;
+            String startTime = entry.toString().substring(startTime_start, startTime_end);
+
+            //Finds the end time in the values of the map
+            int endTime_start = entry.toString().toUpperCase().indexOf("ENDTIME=") + ("ENDTIME=").length();
+            int endTime_end = endTime_start + 5;
+            String endTime = entry.toString().substring(endTime_start, endTime_end);
+
+
+            System.out.println("Event " + count + ": " + title + ", " + startDate + ", " + endDate +
+                    ", " + startTime + ", " + endTime);
+            count+=1;
+        }
+
+
+
+
+
+//                System.out.println(value);
+//                System.out.println(value.toString().toUpperCase().contains("TITLE="));
+//                System.out.println(value.toString().toUpperCase().contains("ENDDATE="));
+//                i.next();
+            }
+
+
 
         entriesSaved.setText("ALL ENTIRES SAVED");
         FadeTransition ft = new FadeTransition(Duration.millis(1850), entriesSaved);
@@ -34,7 +81,7 @@ public class TimetablePresenter {
         ft.setAutoReverse(true);
         ft.play();
         }
-    }
+
     public void loadTODO(GridPane TODO) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LoginPage.class.getClassLoader().getResource("todo.fxml"));
         TODO.getChildren().add(fxmlLoader.load());
