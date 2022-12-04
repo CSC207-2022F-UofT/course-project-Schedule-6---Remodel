a322604774ca8d939dc90fe8a47dcfee41896065
TammyZhuoWang <tammyzhuo.wang@mail.utoronto.ca>
2022-12-04 15:18:08 -0500

    pulled from main to enable saving function

--- src/main/java/screens/TimeTableScreen.java
+++ src/main/java/screens/TimeTableScreen.java
@@ -1,39 +1,29 @@
+
+
 package screens;
 
 import boundary.Import.ImportInputBoundary;
-import com.calendarfx.view.CalendarView;
 import controller.Import.ImportController;
 import controller.Schedule.TimetableController;
 import database.MongoDBAccess;
 import entity.Schedule.CommonScheduleItemFactory;
-import entity.Schedule.ScheduleItem;
 import entity.Schedule.ScheduleItemFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import main.LoginPage;
 import main.collectCollection;
 import presenter.ImportPresenter;
-import requestModel.ImportRequestModel;
-import responseModel.Import.ImportResponseModel;
 import useCaseInteractor.DataAccess;
 import useCaseInteractor.Import.ImportInteractor;
 import useCaseInteractor.User.userCollection;
-import controller.Import.ImportController;
 
 
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Objects;
 
 public class TimeTableScreen {
     @FXML
@@ -52,8 +42,7 @@ public class TimeTableScreen {
     private Button fileImportButton;
     public static Label usernameChangeLabel;
     private Stage filePicker = new Stage();
-
-    private final TimetableController TTC = new TimetableController();
+    private TimetableController TTC = new TimetableController();
 
     private final ImportPresenter presenter = new ImportPresenter();
 
@@ -71,6 +60,7 @@ public class TimeTableScreen {
     private final ImportInputBoundary importInputBoundary= new ImportInteractor(presenter, factory, dataAccess);
 
     private final ImportController importControl = new ImportController(importInputBoundary, presenter);
+
     // private TimetableController timetableController;
     // private ImportController importControl;
 
@@ -81,10 +71,7 @@ public class TimeTableScreen {
     }
      */
 
-    public void showSchedule(ActionEvent event) {
-        TTC.showSchedule(event);
-    }
-    public void printCalendarEntries(ActionEvent event) {TTC.printCalendarEntries(event, allEntriesSavedLabel);}
+    public void printCalendarEntries(ActionEvent event) throws UnknownHostException {TTC.printCalendarEntries(event, allEntriesSavedLabel);}
 
     public void addFutureEntries(ActionEvent event) {TTC.futureEventButton(event);}
     public void addNewFile(ActionEvent event) {importControl.addNewFile(event, fileImportButton, filePicker);}
