8121f3f74ee6dd5cd621a969d44c89ab13d546c5
TammyZhuoWang <tammyzhuo.wang@mail.utoronto.ca>
2022-12-08 23:37:49 -0500

    added tests

--- src/test/java/useCaseInteractor/Import/IcsParserTest.java
+++ src/test/java/useCaseInteractor/Import/IcsParserTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,6 +42,13 @@ public class IcsParserTest {
         FileInputStream f = new FileInputStream("/Users/tammywang/IdeaProjects/course-project-Schedule-6---Remodel/src/test/coursesCalendar.ics");
         IcsParser parser = new IcsParser(f);
         assertEquals(parser.getTitles().get(2), "CSC207H1 LEC5201");
+    }
+
+    @Test
+    void getRRule() throws IOException, ParserException {
+        FileInputStream f = new FileInputStream("/Users/tammywang/IdeaProjects/course-project-Schedule-6---Remodel/src/test/coursesCalendar.ics");
+        IcsParser parser = new IcsParser(f);
+        assertEquals(parser.getRRules().get(0), "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20221207");
     }
 
 }
