0bc09abd3df92bf2e014e043d48f65648fed73c0
nizarabdouss <nizarabdouss@gmail.com>
2022-12-08 21:29:37 -0500

    Tests for responseModels

--- src/test/java/responseModel/Import/ImportResponseModelTest.java
+++ src/test/java/responseModel/Import/ImportResponseModelTest.java
@@ -1,12 +1,22 @@
+<<<<<<< HEAD
 package responseModel.Import;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+=======
+package java.responseModel.Event;
+
+import main.java.responseModel.*;
+import org.junit.jupiter.api.BeforeAll;
+>>>>>>> fed824d (Tests for responseModels)
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+<<<<<<< HEAD
 import java.util.ArrayList;
+=======
+>>>>>>> fed824d (Tests for responseModels)
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ImportResponseModelTest{
 
+<<<<<<< HEAD
     ArrayList<String> titles = new ArrayList<>();
     ArrayList<LocalDate> startDates = new ArrayList<>();
     ArrayList<LocalDate> endDates = new ArrayList<>();
@@ -41,40 +52,76 @@ public class ImportResponseModelTest{
 
     }
     ImportResponseModel test_import = new ImportResponseModel(titles, startDates, endDates, startTimes, endTimes, rr);
+=======
+    @BeforeEach
+    void setUp() {
+    }
+    ImportResponseModel test_import = new ImportResponseModel(["1","2","3"], [LocalDate.now(), LocalDate.now().plusDays(1),
+    LocalDate.now().plusDays(2)], [LocalDate.now(), LocalDate.now().plusDays(2),
+    LocalDate.now().plusDays(2)], [LocalTime.now(), LocalTime.now().plusHours(1), LocalTime.now().plusHours(3)],
+            [LocalTime.now().plusHours(1), LocalTime.now().plusHours(2), LocalTime.now().plusHours(4)],
+            []);
+>>>>>>> fed824d (Tests for responseModels)
 
     @Test
     void Import_Test_GetTitles(){
         assertNotNull(test_import.getTitles());
+<<<<<<< HEAD
         assertTrue(test_import.getTitles() == titles);
+=======
+        assertTrue(test_import.getTitles() == ["1","2","3"]);
+>>>>>>> fed824d (Tests for responseModels)
     }
 
     @Test
     void Import_Test_GetStartDate(){
         assertNotNull(test_import.getStartDates());
+<<<<<<< HEAD
         assertTrue(test_import.getStartDates() == startDates);
+=======
+        assertTrue(test_import.getStartDates() == [LocalDate.now(), LocalDate.now().plusDays(1),
+                LocalDate.now().plusDays(2)]);
+>>>>>>> fed824d (Tests for responseModels)
     }
 
     @Test
     void Import_Test_GetEndDate(){
         assertNotNull(test_import.getEndDates());
+<<<<<<< HEAD
         assertTrue(test_import.getEndDates() == endDates);
+=======
+        assertTrue(test_import.getEndDates() == [LocalDate.now(), LocalDate.now().plusDays(2),
+                LocalDate.now().plusDays(2)]);
+>>>>>>> fed824d (Tests for responseModels)
     }
 
     @Test
     void Import_Test_GetStartTimes(){
         assertNotNull(test_import.getStartTimes());
+<<<<<<< HEAD
         assertTrue(test_import.getStartTimes() == startTimes);
+=======
+        assertTrue(test_import.getStartTimes() == [LocalTime.now(), LocalTime.now().plusHours(1), LocalTime.now().plusHours(3)]);
+>>>>>>> fed824d (Tests for responseModels)
     }
 
     @Test
     void Import_Test_GetEndTimes(){
         assertNotNull(test_import.getEndTimes());
+<<<<<<< HEAD
         assertTrue(test_import.getEndTimes() == endTimes);
+=======
+        assertTrue(test_import.getEndTimes() == [LocalTime.now().plusHours(1), LocalTime.now().plusHours(2), LocalTime.now().plusHours(4)]);
+>>>>>>> fed824d (Tests for responseModels)
     }
 
     @Test
     void Import_Test_getRRules(){
         assertNotNull(test_import.getRRules());
+<<<<<<< HEAD
         assertTrue(test_import.getRRules() == rr);
+=======
+        asserTrue(test_import.getRRules() == []);
+>>>>>>> fed824d (Tests for responseModels)
     }
 }
